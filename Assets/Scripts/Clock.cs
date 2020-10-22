c04ba6f396cc40587a49ff8742f461e03bc627c2
Ralf Herp <65463376+feuerente@users.noreply.github.com>
2020-10-22 12:23:29 +0200

    Add optional continuous rotation to clock

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,14 +7,26 @@ public class Clock : MonoBehaviour
 
     [SerializeField]
     Transform hoursPivot = default, minutesPivot = default, secondsPivot = default;
+    [SerializeField]
+    Boolean continuousRotation = true;
 
     void Update() {
-        DateTime time = DateTime.Now;
-        hoursPivot.localRotation = 
-            Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
-        minutesPivot.localRotation = 
-            Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
-        secondsPivot.localRotation = 
-            Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+        if (continuousRotation) {
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            hoursPivot.localRotation = 
+                Quaternion.Euler(0f, 0f, hoursToDegrees * (float) time.TotalHours);
+            minutesPivot.localRotation = 
+                Quaternion.Euler(0f, 0f, minutesToDegrees * (float) time.TotalMinutes);
+            secondsPivot.localRotation = 
+                Quaternion.Euler(0f, 0f, secondsToDegrees * (float) time.TotalSeconds);
+        } else {
+            DateTime time = DateTime.Now;
+            hoursPivot.localRotation = 
+                Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+            minutesPivot.localRotation = 
+                Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+            secondsPivot.localRotation = 
+                Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+        }
     }
 }
